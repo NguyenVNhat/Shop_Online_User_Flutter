e7bade86f317182e1538eca659821de142c6a091
thanh4001 <ducthanh4000@gmail.com>
2024-10-16 23:16:49 +0700

    C7 : Design SearchUI and Call Api

---
+++ lib/pages/search_page/search_header.dart
@@ -0,0 +1,93 @@
+import 'package:flutter_user_github/data/controller/Product_controller.dart';
+import 'package:flutter_user_github/route/app_route.dart';
+import 'package:flutter_user_github/theme/app_color.dart';
+import 'package:flutter_user_github/theme/app_dimention.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SearchHeader extends StatefulWidget {
+  const SearchHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SearchHeaderState createState() => _SearchHeaderState();
+}
+
+class _SearchHeaderState extends State<SearchHeader> {
+  final TextEditingController searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<ProductController>(builder: (productController) {
+      searchController.text = productController.textSearch;
+      return Container(
+        margin: EdgeInsets.only(
+            top: AppDimention.size40, bottom: AppDimention.size20),
+        padding: EdgeInsets.only(
+            left: AppDimention.size20, right: AppDimention.size20),
+        child: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Get.toNamed(AppRoute.CAMERA_PAGE);
+              },
+              child: Icon(
+                Icons.camera_alt_outlined,
+                color: AppColor.mainColor,
+              ),
+            ),
+            SizedBox(width: AppDimention.size10),
+            Expanded(
+              child: TextField(
+                onChanged: (Text) {
+                  productController.textSearch = searchController.text;
+                  productController.search();
+                },
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: "Search ... ",
+                  prefixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search,
+                      color: AppColor.yellowColor,
+                    ),
+                    onPressed: () {
+                      productController.search();
+                    },
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(AppDimention.size30),
+                    borderSide: BorderSide(width: 1.0, color: Colors.white),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(AppDimention.size30),
+                    borderSide: BorderSide(width: 1.0, color: Colors.white),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(AppDimention.size30),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: AppDimention.size5,
+            ),
+            GestureDetector(
+              onTap: () {
+                Get.toNamed(AppRoute.CART_PAGE);
+              },
+              child: Icon(Icons.shopping_cart_outlined),
+            )
+          ],
+        ),
+      );
+    });
+  }
+}
--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,14 +1,18 @@
-
 import 'package:flutter_user_github/pages/forget_password_page/forget_password_page.dart';
+import 'package:flutter_user_github/pages/home_page/home_page.dart';
 import 'package:flutter_user_github/pages/login_page/login_page.dart';
 import 'package:flutter_user_github/pages/register_page/register_page.dart';
+import 'package:flutter_user_github/pages/search_page/search_page.dart';
 import 'package:flutter_user_github/route/app_route.dart';
 import 'package:get/get.dart';
 
 class AppPages {
   static var list = [
     GetPage(name: AppRoute.LOGIN_PAGE, page: () => LoginPage()),
-     GetPage(name: AppRoute.REGISTER_PAGE, page: () => RegisterPage()),
-     GetPage(name: AppRoute.FORGET_PASSWORD_PAGE, page: () => ForgetPasswordPage()),
+    GetPage(name: AppRoute.REGISTER_PAGE, page: () => RegisterPage()),
+    GetPage(
+        name: AppRoute.FORGET_PASSWORD_PAGE, page: () => ForgetPasswordPage()),
+    GetPage(name: AppRoute.HOME_PAGE, page: () => HomePage()),
+    GetPage(name: AppRoute.SEARCH_PAGE, page: () => SearchPage()),
   ];
 }
