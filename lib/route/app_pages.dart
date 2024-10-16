fe2a1b99d5c0de68b11db50d5f7786583a1e9737
NguyenVNhat <nhataaghjkl@gmail.com>
2024-10-16 23:02:18 +0700

    C3 : Forget function

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter_user_github/pages/forget_password_page/forget_password_page.dart';
 import 'package:flutter_user_github/pages/login_page/login_page.dart';
 import 'package:flutter_user_github/pages/register_page/register_page.dart';
 import 'package:flutter_user_github/route/app_route.dart';
@@ -8,5 +9,6 @@ class AppPages {
   static var list = [
     GetPage(name: AppRoute.LOGIN_PAGE, page: () => LoginPage()),
      GetPage(name: AppRoute.REGISTER_PAGE, page: () => RegisterPage()),
+     GetPage(name: AppRoute.FORGET_PASSWORD_PAGE, page: () => ForgetPasswordPage()),
   ];
 }
