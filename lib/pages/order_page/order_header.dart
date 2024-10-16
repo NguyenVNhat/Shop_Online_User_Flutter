2dee7b8e688c82e590e46b061df5aa856437cfe0
NguyenVNhat <nhataaghjkl@gmail.com>
2024-10-16 23:32:50 +0700

    C13 : Design OrderUI and Call api

---
+++ lib/pages/order_page/order_header.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_user_github/theme/app_color.dart';
+import 'package:flutter_user_github/theme/app_dimention.dart';
+import 'package:flutter/material.dart';
+
+class OrderHeader extends StatefulWidget {
+  const OrderHeader({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _OrderHeaderState createState() => _OrderHeaderState();
+}
+
+class _OrderHeaderState extends State<OrderHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        
+      width: AppDimention.screenWidth,
+      margin: EdgeInsets.only(top: AppDimention.size20),
+      height: AppDimention.size60,
+        child: Center(
+          child: Text(
+            "Đơn hàng của bạn",
+            style: TextStyle(color: AppColor.mainColor, fontSize: 20),
+          ),
+        ));
+  }
+}
