f8338b36b6d0c78b2cc27b2e06eafffa7521cb39
NguyenVNhat <nhataaghjkl@gmail.com>
2024-10-16 23:26:51 +0700

    C10 : Design StoreDetailUI and Call api

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -3,6 +3,7 @@ import 'package:flutter_user_github/pages/home_page/home_page.dart';
 import 'package:flutter_user_github/pages/login_page/login_page.dart';
 import 'package:flutter_user_github/pages/register_page/register_page.dart';
 import 'package:flutter_user_github/pages/search_page/search_page.dart';
+import 'package:flutter_user_github/pages/store_page/store_page.dart';
 import 'package:flutter_user_github/route/app_route.dart';
 import 'package:get/get.dart';
 
@@ -14,5 +15,6 @@ class AppPages {
         name: AppRoute.FORGET_PASSWORD_PAGE, page: () => ForgetPasswordPage()),
     GetPage(name: AppRoute.HOME_PAGE, page: () => HomePage()),
     GetPage(name: AppRoute.SEARCH_PAGE, page: () => SearchPage()),
+    GetPage(name: AppRoute.STORE_PAGE, page: () => StorePage()),
   ];
 }
